951998ab501c5994400acf6103106f373bc66545
Adam Rackis <adamrackis@hotmail.com>
2023-07-13 14:29:05 -0500

    Tweaks

--- src/app/services.ts
+++ src/app/services.ts
@@ -1,19 +1,26 @@
 export async function getA(userId: string = "") {
-  const resp = await fetch("http://localhost:3000/api/a", { next: { revalidate: 10, tags: ["a"] } });
+  const headers: any = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "Cache-Control": "no-cache, no-store, must-revalidate",
+    Pragma: "no-cache",
+    Expires: "0",
+  };
+  const resp = await fetch("http://localhost:5002/api/a", { method: "get", headers, next: { revalidate: 10, tags: ["a"] } });
   const json = await resp.json();
 
   return json;
 }
 
 export async function getB(userId: string = "") {
-  const resp = await fetch("http://localhost:3000/api/b", { next: { revalidate: 10, tags: ["b"] } });
+  const resp = await fetch("http://localhost:5002/api/b", { next: { revalidate: 10, tags: ["b"] } });
   const json = await resp.json();
 
   return json;
 }
 
 export async function getC(userId: string = "") {
-  const resp = await fetch("http://localhost:3000/api/c", { next: { revalidate: 10, tags: ["c"] } });
+  const resp = await fetch("http://localhost:5002/api/c", { next: { revalidate: 10, tags: ["c"] } });
   const json = await resp.json();
 
   return json;
